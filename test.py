1793d1ee066cd15c577c0a517e5ed2c3927d0e03
Matt Sweeney <mattswe@gmail.com>
2013-04-08 11:32:43 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,23 @@
+import time
+from blessings import Terminal
+
+t = Terminal()
+
+print t.bold('Hi there!')
+print t.bold_red_on_bright_green('It hurts my eyes!')
+
+with t.location(0, t.height - 1):
+    print 'This is at the bottom.'
+
+print t.move_down + 'hi'
+time.sleep(1)
+print t.move_down + 'hi'
+time.sleep(1)
+print t.move_down + 'hi'
+time.sleep(1)
+print t.move_down + 'hi'
+time.sleep(1)
+print t.move_down + 'hi'
+time.sleep(1)
+print t.move_down + 'hi'
+time.sleep(3)
